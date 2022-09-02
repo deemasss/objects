8f762a5ddca58c1d41b88dbb5b3439275b432ccc
icedemass <dimon.limon.s90@gmail.com>
2022-09-02 22:06:36 +0300

    Add html and js files

---
+++ index.js
@@ -0,0 +1,31 @@
+const store = {
+    name: 'Vinyl Store',
+    adress: {
+        country: 'UA',
+        city: 'DP',
+        street: 'Gagarin Avenue',
+        houseNumber: 13 
+    },
+    clients: {
+        Elena:{
+            quantity: 8,
+            bonuses: 4
+        },
+        Yuriy:{
+            quantity: 5,
+            bonuses: 2
+        },
+        Mykola:{
+            quantity: 14,
+            bonuses: 6
+        },
+        Alina:{
+            quantity: 4,
+            bonuses: 1
+        },
+        Bohdan:{
+            quantity: 12,
+            bonuses: 5
+        }
+    }
+};
